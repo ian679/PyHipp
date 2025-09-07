ffaec05b0f541810413758dba7d1766b5eaa63ae
AWS ParallelCluster user <ec2-user@ip-10-0-3-114.ap-southeast-1.compute.internal>
2025-09-07 10:40:57 +0000

    envlist script

--- envlist.py
+++ envlist.py
@@ -18,43 +18,32 @@ RESET_MODE = 2
 nargs = len(sys.argv)
 if nargs > 3:
     print('Usage: envlist; envlist env; envlist envprefix nenvs')
-    exit
-elif nargs > 2:
-    # creates or re-creates the list of environments
+    sys.exit()
+elif nargs > 2:    
     pmode = RESET_MODE
-    # get prefix for environment name
     envprefix = sys.argv[1]
-    # get number of environments to create
     nenvs = sys.argv[2]
 elif nargs > 1:
-    # returns env to the list of environments
     pmode = WRITE_MODE
-    # get name of environment to return
     env = sys.argv[1]
 else:
-    # gets name of an environment to use
     pmode = READ_MODE
 
-# creates a lock for the file so it can only be accessed one at a time
+# create a lock for the file
 lock = FileLock(lock_path, timeout=time_out_secs)
 
 with lock:
     if pmode == RESET_MODE:
-        # create a list (named clist) of nevns environments with the 
-        # prefix envprefix
-        # add code here
+        nenvs = int(nenvs)
+        clist = [f"{envprefix}{i}" for i in range(nenvs)]
     else:
-        # load hickle file
         clist = hickle.load(file_path)
 
         if pmode == WRITE_MODE:
-            # append item to end of list
-            # add code here
-        else:    
-            # get and remove env from clist
-            # add code here
-            # return env name
+            clist.append(env)
+        else:  # READ_MODE
+            env = clist.pop(0)
             print(env)
 
-    # save hickle file
     hickle.dump(clist, file_path, mode="w")
+
