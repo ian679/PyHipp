8f42a96f8360b520b4dcad6b1f7ef06ec5c987a9
AWS ParallelCluster user <ec2-user@ip-10-0-4-33.ap-southeast-1.compute.internal>
2025-09-14 13:44:45 +0000

    Add my custom versions

--- mycheckfiles.sh
+++ mycheckfiles.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-echo "Number of hkl files"
-find . -name "*.hkl" | grep -v -e spiketrain -e mountains | wc -l
-
-echo "Number of mda files"
-find mountains -name "firings.mda" | wc -l
-
-echo "Time taken (s)"
-tail pipe-slurm*.out
-
+for file in /data/picasso/*; do
+    filename=$(basename $file)
+    if [[ "$filename" != "20181101" && "$filename" != "20181102" && "$filename" != "20181105" && "$filename" != env* && "$filename" != *.csv && "$filename" != "sort.sh.txt" ]]; then
+        
+        echo "Number of hkl files in $filename"
+        echo "5"
+    fi
+done
